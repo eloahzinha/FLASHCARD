b24e81da0f8292222cc6da9cd26168e63a067278
eloah bautitz <140725424+eloahzinha@users.noreply.github.com>
2024-10-29 08:44:47 -0300

    c

---
+++ perguntas.js
@@ -0,0 +1,55 @@
+criaCartao(
+    'Matemática',
+    'Qual o valor de X sendo: x + 4 = 25 x - 2?',
+    'X: 0,5'
+)
+criaCartao(
+    'Biologia',
+    'Se a hereditariedade de uma pessoa está descrita como : aa, ela tem 100% de chance de ser',
+    'Allbina'
+)
+criaCartao(
+    'Física',
+    'Para que serve a Placa Protoboard ?',
+    'É uma placa que possui furos e conexões internas para montagem de circuitos, utilizada para testes em componentes eletrônicos'
+)
+criaCartao(
+    'Língua Inglesa',
+    'Qual a tradução da frase "Eu estou indo ao shopping"?',
+    'I am going to the mall'
+)
+criaCartao(
+    'Língua Portuguesa',
+    'O que deve constar no 2º parágrafo de um texto dissertativo argumentativo?',
+    'Deve constar a tese'
+)
+criaCartao(
+    'Matemática II',
+    'Para que serve a plataforma Alura?',
+    'Na plataforma Alura constam as instruções do que, e como fazer seus projetos'
+)
+criaCartao(
+    'História',
+    'Em que ano ocorreu a 2ª Guerra Mundial?',
+    'Em 1939'
+)
+criaCartao(
+    'Sociologia',
+    'O que é o Etnocentrismo Cultural?',
+    'É a visão preconceituosa e unilateralmente formada sobre outro povos, culturas, religiões e etnias.'
+)
+criaCartao(
+    'Eduação Financeira',
+    'O que é o Tesouro Direto?',
+    'É uma modalidade de investimento em títulos públicos',
+)
+criaCartao(
+    'Quiímica',
+    'O que é Bondline?',
+    'É uma fórmula que representa a cadeia carbônica em ziguezague formando vértices',
+)
+criaCartao(
+    'Biologia',
+    'Se o homem evoluiu o macaco, por que ainda exitem macacos?',
+    'Porque ele não evoluiu do macaco, mas possuem um ancestral em comum'
+)
