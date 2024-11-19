c885ca45b7db3a5cb8b33db4bd3f901210abf46f
eloah bautitz <140725424+eloahzinha@users.noreply.github.com>
2024-11-19 08:12:20 -0300

    l

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function criaCartao (categoria, pergunta, resposta){
     cartao.className = 'cartao'
 cartao.innerHTML = `
     <div class="cartao__conteudo">
-    <h3>$ {categoria}</h3>
+    <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
         <p>${pergunta}</p>
     </div>
