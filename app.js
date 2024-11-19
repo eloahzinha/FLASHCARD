296217d010bab95baf7415475a1a8e8e5084b397
eloah bautitz <140725424+eloahzinha@users.noreply.github.com>
2024-11-19 08:01:54 -0300

    b

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function criaCartao (categoria, pergunta, resposta){
     let cartao = document.createElement ('article')
     cartao.className = 'cartao'
-    cartao.innerHTML =
+cartao.innerHTML = `
     <div class="cartao__conteudo">
     <h3>$ {categoria}</h3>
     <div class="cartao__conteudo__pergunta">
@@ -11,6 +11,7 @@ function criaCartao (categoria, pergunta, resposta){
         <p>${resposta}</p>
     </div>
 </div>
+`
 let respostaEstaVisivel = false
 function viraCartao(){
     respostaEstaVisivel = !respostaEstaVisivel
